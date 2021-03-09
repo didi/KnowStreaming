d5db028f57225a03da8c3e19f8c0415629f60e65
zengqiao <zengqiao@didiglobal.com>
2021-03-09 15:13:55 +0800

    optimize ldap

--- kafka-manager-extends/kafka-manager-account/src/main/java/com/xiaojukeji/kafka/manager/account/component/sso/BaseSessionSignOn.java
+++ kafka-manager-extends/kafka-manager-account/src/main/java/com/xiaojukeji/kafka/manager/account/component/sso/BaseSessionSignOn.java
@@ -2,7 +2,6 @@ package com.xiaojukeji.kafka.manager.account.component.sso;
 
 import com.xiaojukeji.kafka.manager.account.AccountService;
 import com.xiaojukeji.kafka.manager.account.component.AbstractSingleSignOn;
-import com.xiaojukeji.kafka.manager.common.bizenum.AccountRoleEnum;
 import com.xiaojukeji.kafka.manager.common.constant.LoginConstant;
 import com.xiaojukeji.kafka.manager.common.entity.Result;
 import com.xiaojukeji.kafka.manager.common.entity.ResultStatus;
@@ -10,7 +9,7 @@ import com.xiaojukeji.kafka.manager.common.entity.dto.normal.LoginDTO;
 import com.xiaojukeji.kafka.manager.common.entity.pojo.AccountDO;
 import com.xiaojukeji.kafka.manager.common.utils.EncryptUtil;
 import com.xiaojukeji.kafka.manager.common.utils.ValidateUtils;
-import com.xiaojukeji.kafka.manager.common.utils.ldap.LDAPAuthentication;
+import com.xiaojukeji.kafka.manager.account.component.ldap.LdapAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,19 +27,11 @@ public class BaseSessionSignOn extends AbstractSingleSignOn {
     private AccountService accountService;
 
     @Autowired
-    private LDAPAuthentication ldapAuthentication;
+    private LdapAuthentication ldapAuthentication;
 
     //是否开启ldap验证
-    @Value(value = "${ldap.enabled}")
-    private boolean ldapEnabled;
-
-    //ldap自动注册的默认角色。请注意：它通常来说都是低权限角色
-    @Value(value = "${ldap.auth-user-registration-role}")
-    private String authUserRegistrationRole;
-
-    //ldap自动注册是否开启
-    @Value(value = "${ldap.auth-user-registration}")
-    private boolean authUserRegistration;
+    @Value(value = "${account.ldap.enabled:}")
+    private Boolean accountLdapEnabled;
 
     @Override
     public Result<String> loginAndGetLdap(HttpServletRequest request, HttpServletResponse response, LoginDTO dto) {
@@ -50,27 +41,15 @@ public class BaseSessionSignOn extends AbstractSingleSignOn {
 
         Result<AccountDO> accountResult = accountService.getAccountDO(dto.getUsername());
 
-        //modifier limin
-        //判断是否激活了LDAP验证。若激活并且数据库无此用户则自动注册
-        if(ldapEnabled){
+        //判断是否激活了LDAP验证, 若激活则也可使用ldap进行认证
+        if(!ValidateUtils.isNull(accountLdapEnabled) && accountLdapEnabled){
             //去LDAP验证账密
-            if(!ldapAuthentication.authenricate(dto.getUsername(),dto.getPassword())){
+            if(!ldapAuthentication.authenticate(dto.getUsername(),dto.getPassword())){
                 return Result.buildFrom(ResultStatus.LDAP_AUTHENTICATION_FAILED);
             }
-
-            if((ValidateUtils.isNull(accountResult) || ValidateUtils.isNull(accountResult.getData())) && authUserRegistration){
-                //自动注册
-                AccountDO accountDO = new AccountDO();
-                accountDO.setUsername(dto.getUsername());
-                accountDO.setRole(AccountRoleEnum.getUserRoleEnum(authUserRegistrationRole).getRole());
-                accountDO.setPassword(EncryptUtil.md5(dto.getPassword()));
-                accountService.createAccount(accountDO);
-            }
-
             return Result.buildSuc(dto.getUsername());
-
         }
-        
+
         if (ValidateUtils.isNull(accountResult) || accountResult.failed()) {
             return new Result<>(accountResult.getCode(), accountResult.getMessage());
         }
