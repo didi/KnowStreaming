7719339f2385f59bf606f268d4d8ac5793be4129
zengqiao <zengqiao@didiglobal.com>
2021-05-18 20:18:08 +0800

    split op util controller to topic controller and leader controller, and add authority controller, quota controller

--- kafka-manager-common/src/main/java/com/xiaojukeji/kafka/manager/common/entity/dto/gateway/TopicQuotaDTO.java
+++ kafka-manager-common/src/main/java/com/xiaojukeji/kafka/manager/common/entity/dto/gateway/TopicQuotaDTO.java
@@ -7,46 +7,41 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description = "配额调整")
 public class TopicQuotaDTO extends ClusterTopicDTO {
+    @ApiModelProperty(value = "appId")
+    private String appId;
 
-  @ApiModelProperty(value = "appId")
-  private String appId;
+    @ApiModelProperty(value = "发送数据速率B/s")
+    private Long produceQuota;
 
-  @ApiModelProperty(value = "发送数据速率B/s")
-  private Long produceQuota;
+    @ApiModelProperty(value = "消费数据速率B/s")
+    private Long consumeQuota;
 
-  @ApiModelProperty(value = "消费数据速率B/s")
-  private Long consumeQuota;
-
-  public String getAppId() {
+    public String getAppId() {
     return appId;
   }
 
-  public void setAppId(String appId) {
+    public void setAppId(String appId) {
     this.appId = appId;
   }
 
-  public Long getProduceQuota() {
+    public Long getProduceQuota() {
     return produceQuota;
   }
 
-  public void setProduceQuota(Long produceQuota) {
+    public void setProduceQuota(Long produceQuota) {
     this.produceQuota = produceQuota;
   }
 
-  public Long getConsumeQuota() {
+    public Long getConsumeQuota() {
     return consumeQuota;
   }
 
-  public void setConsumeQuota(Long consumeQuota) {
+    public void setConsumeQuota(Long consumeQuota) {
     this.consumeQuota = consumeQuota;
   }
 
-  public boolean paramLegal() {
-    if (ValidateUtils.isNullOrLessThanZero(clusterId)
-          || ValidateUtils.isBlank(topicName)
-          || ValidateUtils.isBlank(appId)) {
-      return false;
+    @Override
+    public boolean paramLegal() {
+        return !ValidateUtils.isNullOrLessThanZero(clusterId) && !ValidateUtils.isBlank(topicName) && !ValidateUtils.isBlank(appId);
     }
-    return true;
-  }
 }
