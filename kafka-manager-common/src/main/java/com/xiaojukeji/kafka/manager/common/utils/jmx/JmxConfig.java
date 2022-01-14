52c52b2a0df956541c2797c72f36e06bd2ceed20
zengqiao <zengqiao@didiglobal.com>
2022-01-14 14:24:04 +0800

    增加简单回退工具类，增加Jmx连接失败回退功能机制，优化Jmx连接失败日志

--- kafka-manager-common/src/main/java/com/xiaojukeji/kafka/manager/common/utils/jmx/JmxConfig.java
+++ kafka-manager-common/src/main/java/com/xiaojukeji/kafka/manager/common/utils/jmx/JmxConfig.java
@@ -1,5 +1,10 @@
 package com.xiaojukeji.kafka.manager.common.utils.jmx;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class JmxConfig {
     /**
      * 单台最大连接数
@@ -21,45 +26,8 @@ public class JmxConfig {
      */
     private Boolean openSSL;
 
-    public Integer getMaxConn() {
-        return maxConn;
-    }
-
-    public void setMaxConn(Integer maxConn) {
-        this.maxConn = maxConn;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Boolean isOpenSSL() {
-        return openSSL;
-    }
-
-    public void setOpenSSL(Boolean openSSL) {
-        this.openSSL = openSSL;
-    }
-
-    @Override
-    public String toString() {
-        return "JmxConfig{" +
-                "maxConn=" + maxConn +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", openSSL=" + openSSL +
-                '}';
-    }
+    /**
+     * 连接重试回退事件
+     */
+    private Long retryConnectBackoffTimeUnitMs;
 }
