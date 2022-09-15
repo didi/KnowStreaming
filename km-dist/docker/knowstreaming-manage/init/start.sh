27d1b92690be3762466ae9bbbc7d64c4c8d45c7f
haoqi <1148648445@qq.com>
2022-09-15 16:22:51 +0800

    1. 添加init容器，只用于初始化es索引

--- km-dist/docker/knowstreaming-manage/init/start.sh
+++ km-dist/docker/knowstreaming-manage/init/start.sh
@@ -1,5 +1,3 @@
-/bin/bash /template.sh start
-
 sed -i "s/SERVER_MYSQL_ADDRESS/${SERVER_MYSQL_ADDRESS}/g" /conf/application.yml
 sed -i "s/SERVER_MYSQL_DB/${SERVER_MYSQL_DB}/g" /conf/application.yml
 sed -i "s/SERVER_MYSQL_USER/${SERVER_MYSQL_USER}/g" /conf/application.yml
