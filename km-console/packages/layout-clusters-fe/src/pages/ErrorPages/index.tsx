1a5e2c7309514cfe5f46cec5f2d2a2210158200c
GraceWalk <gracewalk1999@163.com>
2022-10-28 17:36:33 +0800

    fix: 错误页面优化

---
+++ km-console/packages/layout-clusters-fe/src/pages/ErrorPages/index.tsx
@@ -0,0 +1,4 @@
+import './index.less';
+export { default as Page403 } from './Page403';
+export { default as Page404 } from './Page404';
+export { default as NoLicense } from './NoLicense';
--- km-console/packages/layout-clusters-fe/src/pages/NoLicense/index.tsx
+++
@@ -1,4 +0,0 @@
-import { Result } from 'knowdesign';
-import React from 'react';
-
-export default () => <Result status="403" title="No License" subTitle="很抱歉，您的 Licence 无法使用" />;
