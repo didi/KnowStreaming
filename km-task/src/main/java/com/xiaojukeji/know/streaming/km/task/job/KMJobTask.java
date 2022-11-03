d96b6bdc44866e11f7fae473dd140e7c9ada989e
Richard <49510754+f1558@users.noreply.github.com>
2022-11-03 17:30:00 +0800

    fix issue  *[issue #743] Fix issue about a request that cannot be found for thread binding.

--- km-task/src/main/java/com/xiaojukeji/know/streaming/km/task/job/KMJobTask.java
+++ km-task/src/main/java/com/xiaojukeji/know/streaming/km/task/job/KMJobTask.java
@@ -7,6 +7,9 @@ import com.xiaojukeji.know.streaming.km.common.bean.entity.cluster.ClusterPhy;
 import com.xiaojukeji.know.streaming.km.core.service.job.JobService;
 import com.xiaojukeji.know.streaming.km.task.AbstractAsyncCommonDispatchTask;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Task(name = "kmJobTask",
         description = "km job 模块调度执行任务",
@@ -21,6 +24,12 @@ public class KMJobTask extends AbstractAsyncCommonDispatchTask {
 
     @Override
     public TaskResult processClusterTask(ClusterPhy clusterPhy, long triggerTimeUnitMs) throws Exception {
+        // fix request that cannot find thread binding (issue#743)
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (null == servletRequestAttributes) {
+            servletRequestAttributes = new ServletRequestAttributes(new MockHttpServletRequest());
+        }
+        RequestContextHolder.setRequestAttributes(servletRequestAttributes, true);
         jobService.scheduleJobByClusterId(clusterPhy.getId());
         return TaskResult.SUCCESS;
     }
