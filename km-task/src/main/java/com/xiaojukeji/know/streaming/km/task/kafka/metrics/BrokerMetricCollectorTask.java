7066246e8f8fd0a247b02fcd0417d641f08767ba
zengqiao <zengqiao@didiglobal.com>
2022-12-05 13:49:35 +0800

    [Optimize]错开采集任务触发时间，降低Offset信息获取时超时情况的发生(#726)
    当前指标采集任务都是整分钟触发执行的，导致会同时向Kafka请求分区Offset信息，会导致：
    1、请求过多，从而出现超时；
    2、同时进行，可能会导致分区重复获取Offset信息；
    
    因此将其错开。

--- km-task/src/main/java/com/xiaojukeji/know/streaming/km/task/kafka/metrics/BrokerMetricCollectorTask.java
+++ km-task/src/main/java/com/xiaojukeji/know/streaming/km/task/kafka/metrics/BrokerMetricCollectorTask.java
@@ -3,8 +3,6 @@ package com.xiaojukeji.know.streaming.km.task.kafka.metrics;
 import com.didiglobal.logi.job.annotation.Task;
 import com.didiglobal.logi.job.common.TaskResult;
 import com.didiglobal.logi.job.core.consensual.ConsensualEnum;
-import com.didiglobal.logi.log.ILog;
-import com.didiglobal.logi.log.LogFactory;
 import com.xiaojukeji.know.streaming.km.collector.metric.kafka.BrokerMetricCollector;
 import com.xiaojukeji.know.streaming.km.common.bean.entity.cluster.ClusterPhy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @Task(name = "BrokerMetricCollectorTask",
         description = "Broker指标采集任务",
-        cron = "0 0/1 * * * ? *",
+        cron = "20 0/1 * * * ? *",
         autoRegister = true,
         consensual = ConsensualEnum.BROADCAST,
         timeout = 2 * 60)
 public class BrokerMetricCollectorTask extends AbstractAsyncMetricsDispatchTask {
-    private static final ILog log = LogFactory.getLog(BrokerMetricCollectorTask.class);
-
     @Autowired
     private BrokerMetricCollector brokerMetricCollector;
 
