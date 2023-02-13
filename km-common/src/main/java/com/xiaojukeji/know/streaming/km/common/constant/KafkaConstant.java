5db722a465a2697b01b138b3dad2465ffebb0d25
zengqiao <zengqiao@didiglobal.com>
2023-02-13 11:18:29 +0800

    [Bugfix]修复 Broker Similar Config 显示错误的问题(#872)

--- km-common/src/main/java/com/xiaojukeji/know/streaming/km/common/constant/KafkaConstant.java
+++ km-common/src/main/java/com/xiaojukeji/know/streaming/km/common/constant/KafkaConstant.java
@@ -45,6 +45,8 @@ public class KafkaConstant {
 
     public static final String DEFAULT_CONNECT_VERSION = "2.5.0";
 
+    public static final List<String> CONFIG_SIMILAR_IGNORED_CONFIG_KEY_LIST = Arrays.asList("broker.id", "listeners", "name", "value", "advertised.listeners", "node.id");
+
     public static final Map<String, ConfigDef.ConfigKey> KAFKA_ALL_CONFIG_DEF_MAP = new ConcurrentHashMap<>();
 
     static {
