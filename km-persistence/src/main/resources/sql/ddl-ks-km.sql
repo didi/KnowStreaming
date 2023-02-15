9fa78f78a34fa0316e3b1b747b874684a978f4b1
shizeying <w741069229@163.com>
2023-02-15 14:32:22 +0800

    [Bugfix]新增idx_cluster_update_time(https://github.com/didi/KnowStreaming/issues/918)

--- km-persistence/src/main/resources/sql/ddl-ks-km.sql
+++ km-persistence/src/main/resources/sql/ddl-ks-km.sql
@@ -198,7 +198,9 @@ CREATE TABLE `ks_km_kafka_change_record` (
   UNIQUE KEY `unique_field` (`unique_field`),
   KEY `idx_cluster_phy_id` (`cluster_phy_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Kafka变更记录表';
-
+ALTER TABLE `ks_km_kafka_change_record`
+DROP INDEX `idx_cluster_phy_id` ,
+ADD INDEX `idx_cluster_update_time` (`cluster_phy_id` ASC, `update_time` ASC);
 
 
 DROP TABLE IF EXISTS `ks_km_kafka_controller`;
