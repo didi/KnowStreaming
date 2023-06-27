623954981d40a408317d73a3ba1c1d40be041f89
ZQKC <zengqiao_cn@163.com>
2023-06-27 14:01:53 +0800

    [Feature]新增删除Group或GroupOffset功能
    不包括前端，后端新增
    1、新增Group删除功能；
    2、新增Group-Topic纬度Offset删除功能；
    3、新增Group-Topic-Partition纬度Offset删除功能；

--- km-common/src/main/java/com/xiaojukeji/know/streaming/km/common/bean/entity/param/group/GroupParam.java
+++ km-common/src/main/java/com/xiaojukeji/know/streaming/km/common/bean/entity/param/group/GroupParam.java
@@ -1,13 +1,11 @@
 package com.xiaojukeji.know.streaming.km.common.bean.entity.param.group;
 
 import com.xiaojukeji.know.streaming.km.common.bean.entity.param.cluster.ClusterPhyParam;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class GroupParam extends ClusterPhyParam {
     protected String groupName;
 
