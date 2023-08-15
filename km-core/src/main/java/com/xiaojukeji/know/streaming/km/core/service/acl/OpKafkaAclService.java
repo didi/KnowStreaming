46dcf6e8802a21e1c83b5e9ee88841978691b943
ZQKC <zengqiao_cn@163.com>
2023-08-15 18:38:45 +0800

    [Optimize]统一DB元信息更新格式-Part2
    1、KafkaMetaService修改为MetaService并移动到Core层；
    2、修改ZK、KafkaACL的格式；

--- km-core/src/main/java/com/xiaojukeji/know/streaming/km/core/service/acl/OpKafkaAclService.java
+++ km-core/src/main/java/com/xiaojukeji/know/streaming/km/core/service/acl/OpKafkaAclService.java
@@ -3,10 +3,6 @@ package com.xiaojukeji.know.streaming.km.core.service.acl;
 import com.xiaojukeji.know.streaming.km.common.bean.entity.param.acl.ACLAtomParam;
 import com.xiaojukeji.know.streaming.km.common.bean.entity.result.Result;
 import com.xiaojukeji.know.streaming.km.common.bean.po.KafkaAclPO;
-import org.apache.kafka.common.resource.ResourceType;
-
-import java.util.Date;
-import java.util.List;
 
 public interface OpKafkaAclService {
     /**
@@ -19,14 +15,5 @@ public interface OpKafkaAclService {
      */
     Result<Void> deleteKafkaAcl(ACLAtomParam aclAtomParam, String operator);
 
-    /**
-     * 删除ACL
-     */
-    Result<Void> deleteKafkaAclByResName(ResourceType resourceType, String resourceName, String operator);
-
     Result<Void> insertAndIgnoreDuplicate(KafkaAclPO kafkaAclPO);
-
-    void batchUpdateAcls(Long clusterPhyId, List<KafkaAclPO> poList);
-
-    int deleteByUpdateTimeBeforeInDB(Long clusterPhyId, Date beforeTime);
 }
--- km-task/src/main/java/com/xiaojukeji/know/streaming/km/task/kafka/metadata/SyncKafkaAclTask.java
+++ km-task/src/main/java/com/xiaojukeji/know/streaming/km/task/kafka/metadata/SyncKafkaAclTask.java
@@ -3,19 +3,13 @@ package com.xiaojukeji.know.streaming.km.task.kafka.metadata;
 import com.didiglobal.logi.job.annotation.Task;
 import com.didiglobal.logi.job.common.TaskResult;
 import com.didiglobal.logi.job.core.consensual.ConsensualEnum;
-import com.didiglobal.logi.log.ILog;
-import com.didiglobal.logi.log.LogFactory;
 import com.xiaojukeji.know.streaming.km.common.bean.entity.cluster.ClusterPhy;
 import com.xiaojukeji.know.streaming.km.common.bean.entity.result.Result;
-import com.xiaojukeji.know.streaming.km.common.bean.po.KafkaAclPO;
-import com.xiaojukeji.know.streaming.km.common.converter.KafkaAclConverter;
 import com.xiaojukeji.know.streaming.km.core.service.acl.KafkaAclService;
-import com.xiaojukeji.know.streaming.km.core.service.acl.OpKafkaAclService;
 import org.apache.kafka.common.acl.AclBinding;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Task(name = "SyncKafkaAclTask",
         description = "KafkaAcl信息同步到DB",
@@ -24,32 +18,18 @@ import java.util.stream.Collectors;
         consensual = ConsensualEnum.BROADCAST,
         timeout = 2 * 60)
 public class SyncKafkaAclTask extends AbstractAsyncMetadataDispatchTask {
-    private static final ILog log = LogFactory.getLog(SyncKafkaAclTask.class);
-
     @Autowired
     private KafkaAclService kafkaAclService;
 
-    @Autowired
-    private OpKafkaAclService opKafkaAclService;
-
     @Override
     public TaskResult processClusterTask(ClusterPhy clusterPhy, long triggerTimeUnitMs) {
-        Result<List<AclBinding>> aclBindingListResult = kafkaAclService.getAclFromKafka(clusterPhy.getId());
+        Result<List<AclBinding>> aclBindingListResult = kafkaAclService.getDataFromKafka(clusterPhy);
         if (aclBindingListResult.failed()) {
             return TaskResult.FAIL;
         }
 
-        if (!aclBindingListResult.hasData()) {
-            return TaskResult.SUCCESS;
-        }
+        kafkaAclService.writeToDB(clusterPhy.getId(), aclBindingListResult.getData());
 
-        // 更新DB数据
-        List<KafkaAclPO> poList = aclBindingListResult.getData()
-                .stream()
-                .map(elem -> KafkaAclConverter.convert2KafkaAclPO(clusterPhy.getId(), elem, triggerTimeUnitMs))
-                .collect(Collectors.toList());
-
-        opKafkaAclService.batchUpdateAcls(clusterPhy.getId(), poList);
         return TaskResult.SUCCESS;
     }
 }
